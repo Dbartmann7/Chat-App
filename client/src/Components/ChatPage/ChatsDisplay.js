5ba5975639840950a88a5cd5329c13c7040a8d2a
Daniel <dbartmann16@gmail.com>
2023-07-15 15:51:25 +0100

    more updates to UI

--- client/src/Components/ChatPage/ChatsDisplay.js
+++ client/src/Components/ChatPage/ChatsDisplay.js
@@ -2,14 +2,14 @@ import React from "react"
 import "./ChatDisplay.css"
 import Chat from "./Chat"
 const ChatsDisplay = (props) => {
-    const {clientID, messages} = props
+    const {messages} = props
 
     console.log(messages)
     return(<>
         <div className="ChatsDisplayBox">
           <div className="ChatsContainer">
             <ul>
-              {messages.map((message, i) => {
+              {messages.toReversed().map((message, i) => {
                   return(<Chat key={i} clientID={message.username} message={message.body} timeSent={message.timeSent}/>)
               })}
             </ul>
